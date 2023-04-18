ba341972fdfa154dd02823252ffc45a6e49a15c5
10xwannabe <alexey.kalmykoff@gmail.com>
2023-04-18 20:12:03 +0300

    minor

--- references/keccak.py
+++ references/keccak.py
@@ -12,21 +12,21 @@ if __name__ == "__main__":
     hex_chars = group_by_bytes*2
     for i in range(len(hexdigest)//hex_chars):
         start_i = hex_chars*i
-        print(f"{i}: {hexdigest[start_i:start_i+(hex_chars)]}")
-    print("--- decimals (big endian):")
+        print(f"uint64 {i}: {hexdigest[start_i:start_i+(hex_chars)]}")
+    print("--- decimals (big endian):")  # 00000000000000013   0x00000000000000013
     digest = k.digest()
     for i in range(len(digest)//group_by_bytes):
         start_i = group_by_bytes*i
-        print(f"{i}: {int.from_bytes(digest[start_i:start_i+(group_by_bytes)], byteorder='big')}")
-    print("--- decimals (little endian):")
+        print(f"uint64 {i}: {int.from_bytes(digest[start_i:start_i+(group_by_bytes)], byteorder='big')}")
+    print("--- decimals (little endian):")  # 3100000000000000000   0x13000000000000000
     for i in range(len(digest)//group_by_bytes):
         start_i = group_by_bytes*i
-        print(f"{i}: {int.from_bytes(digest[start_i:start_i+(group_by_bytes)], byteorder='little')}")
+        print(f"uint64  {i}: {int.from_bytes(digest[start_i:start_i+(group_by_bytes)], byteorder='little')}")
 
 
     print(f"0x80 big: {int.from_bytes([0,0,0,0,0,0,0,0x80], byteorder='big')}")
     print(f"0x80 little: {int.from_bytes([0,0,0,0,0,0,0,0x80], byteorder='little')}") # 9223372036854775808
-
+                                         # 0x8000000000000000 = 9223372036854775808
 # [17376452488221285863 9571781953733019530 15391093639620504046 13624874521033984333 10027350355371872343 18417369716475457492 10448040663659726788 10113917136857017974 12479658147685402012 3500241080921619556 16959053435453822517 12224711289652453635 9342009439668884831 4879704952849025062 140226327413610143 424854978622500449 7259519967065370866 7004910057750291985 13293599522548616907 10105770293752443592 10668034807192757780 1747952066141424100 1654286879329379778 8500057116360352059 16929593379567477321]
 
 
